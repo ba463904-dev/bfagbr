c685d56b7d3fec6178840fc14b729a8d4a0cacd2
Ariz Bautista <ba463904@uaeh.edu.mx>
2026-02-26 17:58:56 -0600

    Update __init__.py

--- bfagbr/__init__.py
+++ bfagbr/__init__.py
@@ -11,3 +11,24 @@ def is_3_colorable(graph):
     if is_proper_coloring(graph, coloring):
       return coloring
   return False
+
+
+##############################################
+
+import itertools
+
+def sum_of_values(values, keys):
+    sum = 0
+    n = len(values)
+    for i in range(n):
+        sum+=values[i]*keys[i]
+    return sum
+
+def knapsack_problem(profits, weights, capacity, goal):
+    n = len(profits)
+    sequences = itertools.product([0, 1], repeat = n)
+    for sequence in sequences:
+        if sum_of_values(weights, sequence) <= capacity: 
+            if sum_of_values(profits, sequence)>=goal:
+                return sequence
+    return False
